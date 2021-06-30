aa4754d214b4d222f1ddcf9ab15f54dd9a084eed
szandany <44092058+szandany@users.noreply.github.com>
2021-06-30 08:57:41 -0500

    Update changelog_version-3.2.h2.sql

--- src/main/resources/db/changelog/changelog_version-3.2.h2.sql
+++ src/main/resources/db/changelog/changelog_version-3.2.h2.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
---changeset SteveZ:1582471835439-1
+--changeset SteveZ:1
 CREATE TABLE SALES (ID BIGINT AUTO_INCREMENT, ITEM VARCHARS(255), QUANTITY NUMBER, AMOUNT FLOAT, CONSTRAINT SALES_PK PRIMARY KEY (ID));
 --rollback DROP TABLE SALES;
 
---changeset JohnD:15824-2
-INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Leather sofa By Michellotti', 4, 5026.63);
+--changeset JohnD:2
+INSERT INTO SALES (ITEMS, QUANTITY, AMOUNT) VALUES ('Leather sofa By Michellotti', 4, 5026.63);
 --rollback DELETE FROM SALES WHERE ITEM='Leather sofa By Michellotti';
 
---changeset JohnD:1582471835439-6
-INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Round coffee table By Michellotti', 1, 800.23);
+--changeset JohnD:3
+INSERT INTO SALES (ITEM, QUANTITY, AMOUNTS) VALUES ('Round coffee table By Michellotti', 1, 800.23);
 --rollback DELETE FROM SALES WHERE ITEM='Round coffee table By Michellotti';
